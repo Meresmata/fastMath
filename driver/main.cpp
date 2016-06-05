78401cdd9c59d3d7eb5a96e0857da5291aac76f1
Meresmata <tetje@yandex.ru>
2016-06-05 21:32:48 +0200

    smaller changes in the driver folder

--- driver/main.cpp
+++ driver/main.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 
 int i = 0;
-double ii = 0.0;
-double diff = 0.0;
-double maxdiff = 0.0;
-double sumdiff = 0.0;
-double avediff = 0.0;
-double maxDiffArc = 0.0;
+float ii = 0.0;
+float diff = 0.0;
+float maxdiff = 0.0;
+float sumdiff = 0.0;
+float avediff = 0.0;
+float maxDiffArc = 0.0;
 
-void mathBench(double(*fn_ptr)(double, double), const double x, const double y) {
-	int d = static_cast<int>(fastmath::toDeg((*fn_ptr)(x, y)) * 100);
+void mathBench(float(*fn_ptr)(float, float), const float x, const float y) {
+	int d = static_cast<int>(fmath::toDeg((*fn_ptr)(x, y)) * 100);
 	char buf[51];
 	snprintf(buf, sizeof(buf), "Fkt. %d.%2d degrees", d / 100, d % 100);
 	cout << buf <<endl;
@@ -26,8 +26,8 @@ int main() {
 
 
     for (i=0;i < 360; i++ ) {
-		ii = i - 180;
-		diff = abs(cos(fastmath::toRad(ii)) - fastmath::cos(fastmath::toRad(ii)));
+		ii = static_cast<float>(i - 180);
+		diff = abs(cosf(fmath::toRad(ii)) - fmath::cos(fmath::toRad(ii)));
 		sumdiff += diff;
 		if (diff > maxdiff)
 			maxdiff = diff;
@@ -39,9 +39,9 @@ int main() {
 	cout << maxdiff << avediff << endl;
 
 	for (i = 0; i < 350; i++) {
-		ii = static_cast<double>(i-175) / 10.0;
-		double libArc = fastmath::toDeg(atan(ii));
-		double ownArc = fastmath::toDeg(fastmath::atan(ii));
+		ii = static_cast<float>(i-175) / 10.0F;
+		float libArc = fmath::toDeg(atanf(ii));
+		float ownArc = fmath::toDeg(fmath::atan(ii));
 		diff = abs(libArc - ownArc);
 		sumdiff += diff;
 		if (diff > maxdiff){
@@ -56,11 +56,11 @@ int main() {
 
 
 	for (i = 0; i < 350; i++) {
-		ii = static_cast<double>(i - 175) / 1;
+		ii = static_cast<float>(i - 175) / 1;
 		for (int j = 0; j < 35; j++) {
-			double jj = static_cast<double>(i - 175) / 1;
-			mathBench((atan2), (ii), (jj));
-			mathBench((fastmath::atan2), (ii), (jj));
+			float jj = static_cast<float>(i - 175) / 1;
+			mathBench((atan2f), (ii), (jj));
+			mathBench((fmath::atan2), (ii), (jj));
 			/*double libArc = fastToDeg(atan2(ii, jj));
 			double ownArc = fastToDeg(fastAtan2(ii, jj));
 			diff = abs(libArc - ownArc);
